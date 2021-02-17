2d54bc01ed87916f445f75d97807320e7478f3af
Beth Jackson <Mbeth.ashton@gmail.com>
2021-02-16 19:07:11 -0500

    Somewhere in Chp 4

--- test/mango_web/acceptance/category_page_test.exs
+++ test/mango_web/acceptance/category_page_test.exs
@@ -13,10 +13,54 @@ defmodule Mangoeb.Acceptance.CategoryPageTest do
   end
   
   test "show fruits" do
-    # write test for fruits
+    ## WHEN ##
+    # I navigate to the fruits page
+    navigate_to("/categories/fruits")
+    
+    ## THEN ##
+    # I expect the page title to be "Fruits"
+    page_title = find_element(:css, ".page-title") 
+      |> visible_text()
+      
+    assert page_title == "Fruits"
+    
+    # And I expect Apple in the products displayed
+    product = find_element(:css, ".product")
+    product_name = find_within_element(product, :css, ".product-name")
+      |> visible_text()
+    product_price = find_within_element(product, :css, ".product-price")
+      |> visible_text()
+      
+    assert product_name == "Apple"
+    assert product_price == "100"
+    
+    # And I expect that Tomato is not present on screen
+    refute page_source() =~ "Tomato"
   end
   
   test "show vegetables" do
-    # write test for vegetables
+    ## WHEN ##
+    # I navigate to the vegetables page
+    navigate_to("/categories/vegetables")
+    
+    ## THEN ##
+    # I expect the page title to be "Vegetables"
+    page_title = find_element(:css, ".page-title") 
+      |> visible_text()
+      
+    assert page_title == "Vegetables"
+    
+    # And I expect Tomato in the products displayed
+    product = find_element(:css, ".product")
+    product_name = find_within_element(product, :css, ".product-name")
+      |> visible_text()
+    product_price = find_within_element(product, :css, ".product-price")
+      |> visible_text()
+      
+    assert product_name == "Tomato"
+    assert product_price == "50"
+    
+    # And I expect that Apple is not present on screen
+    refute page_source() =~ "Apple"
   end
 end
