970a660bebac3139877d01ab8d97542fd7ba865f
Beth Jackson <Mbeth.ashton@gmail.com>
2021-03-16 15:15:37 -0400

    pg 146

--- lib/mango/crm/customer.ex
+++ lib/mango/crm/customer.ex
@@ -2,25 +2,21 @@ defmodule Mango.CRM.Customer do
   use Ecto.Schema
   import Ecto.Changeset
   alias Mango.CRM.Customer
-
   schema "customers" do
     field :email, :string
     field :name, :string
-    field :password, :string, virtual: true
     field :password_hash, :string
     field :phone, :string
     field :residence_area, :string
-
     timestamps()
   end
-
   @doc false
   def changeset(%Customer{} = customer, attrs) do
     customer
-    |> cast(attrs, [:name, :email, :phone, :residence_area, :password])
-    |> validate_required([:name, :email, :phone, :residence_area, :password])
-    |> validate_format(:email, ~r/@/, message: "is invalid")
-    |> validate_length(:password, min: 6, max: 100)
+    |> cast(attrs, [:name, :email, :phone, :residence_area,
+    :password_hash])
+    |> validate_required([:name, :email, :phone, :residence_area,
+    :password_hash])
     |> unique_constraint(:email)
   end
 end
