1545ff9448ec420ce7fc75929d7eef8c1d81cd30
Beth Jackson <Mbeth.ashton@gmail.com>
2021-03-23 18:44:54 -0400

    pg 181

--- lib/mango_web/controllers/session_controller.ex
+++ lib/mango_web/controllers/session_controller.ex
@@ -4,4 +4,10 @@ defmodule MangoWeb.SessionController do
   def new(conn, _params) do
     render(conn, "new.html")
   end
+  
+  def create(conn, %{"session" => session_params}) do
+    # check and load the customer matching the given credentials
+    # if customer found, put the customer id on session data
+    # if not found return to login page with error
+  end
 end
