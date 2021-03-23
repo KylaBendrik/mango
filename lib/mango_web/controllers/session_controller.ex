69937a468d18e0c925e0f1da61c1d94ccd9f5303
Beth Jackson <Mbeth.ashton@gmail.com>
2021-03-23 19:16:12 -0400

    pg 184

--- lib/mango_web/controllers/session_controller.ex
+++ lib/mango_web/controllers/session_controller.ex
@@ -9,5 +9,18 @@ defmodule MangoWeb.SessionController do
     # check and load the customer matching the given credentials
     # if customer found, put the customer id on session data
     # if not found return to login page with error
+    case CRM.get_customer_by_credentials(session_params) do
+      :error ->
+        conn
+        |> put_flast(:error, "Invalid username/password combination")
+        |> render("new.html")
+      customer ->
+        conn
+        |> assign(:current_customer, customer)
+        |> put_session(:customer_id, customer.id)
+        |> configure_session(renew: true)
+        |> put_flash(:info, "Login successful")
+        |> redirect(to: page_path(conn, :index))
+    end
   end
 end
