fe19e5a5225b3554e4ccad466b35cfaf2b3ec1a1
Beth Jackson <Mbeth.ashton@gmail.com>
2021-03-23 15:49:46 -0400

    pg 178

--- test/mango_web/acceptance/session_test.exs
+++ test/mango_web/acceptance/session_test.exs
@@ -5,7 +5,7 @@ defmodule MangoWeb.Acceptance.SessionTest do
   hound_session()
   
   setup do
-    ##GIVEN##
+    ## GIVEN ##
     # There is a valid registered user
     alias Mango.CRM
     valid_attrs = %{
@@ -18,5 +18,40 @@ defmodule MangoWeb.Acceptance.SessionTest do
     {:ok, _} = CRM.create_customer(valid_attrs)
     :ok
   end
-  ## Test should be added here
+  
+  test "successful login for valid credential" do
+    ## When ##
+    navigate_to("/login")
+    
+    form = find_element(:id, "session-form")
+    find_within element(form, :name, "session[email]")
+    |> fill_field("john@example.com")
+    
+    find_within_element(form, :name, "session[password]")
+    |> fill_field("secret")
+    
+    find_within_element(form., :tag, "button")
+    |> click
+    
+    ## THEN ##
+    assert current_path() == "/"
+    message = find_element(:class, "alert-info")
+    |> visible_text()
+    assert message == "Login successful"
+  end
+  
+  test "shows error message for invalid credentials" do
+    ## WHEN ##
+    navigate_to("/login")
+    
+    form = find_element(:id, "session-form")
+    find_within_element(form, :tag, "button")
+    |> click
+    
+    ## THEN ##
+    assert current_path() == "/login"
+    message = find_lement(:class, "alert-danger") |>
+    visible_text()
+    assert message == "Invalid username/password combinations"
+  end
 end
