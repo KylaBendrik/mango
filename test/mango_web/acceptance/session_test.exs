e62757b62963a3eaaef37b97fe21f73b0d42e1a2
Sojourn4 <58755132+Sojourn4@users.noreply.github.com>
2021-03-30 19:08:07 -0400

    make a koo

--- test/mango_web/acceptance/session_test.exs
+++ test/mango_web/acceptance/session_test.exs
@@ -1,9 +1,9 @@
 defmodule MangoWeb.Acceptance.SessionTest do
   use Mango.DataCase
   use Hound.Helpers
-  
+
   hound_session()
-  
+
   setup do
     ## GIVEN ##
     # There is a valid registered user
@@ -18,40 +18,73 @@ defmodule MangoWeb.Acceptance.SessionTest do
     {:ok, _} = CRM.create_customer(valid_attrs)
     :ok
   end
-  
+
   test "successful login for valid credential" do
     ## When ##
     navigate_to("/login")
-    
+
     form = find_element(:id, "session-form")
     find_within element(form, :name, "session[email]")
     |> fill_field("john@example.com")
-    
+
     find_within_element(form, :name, "session[password]")
     |> fill_field("secret")
-    
+
     find_within_element(form., :tag, "button")
     |> click
-    
+
     ## THEN ##
     assert current_path() == "/"
     message = find_element(:class, "alert-info")
     |> visible_text()
     assert message == "Login successful"
   end
-  
+
   test "shows error message for invalid credentials" do
     ## WHEN ##
     navigate_to("/login")
-    
+
     form = find_element(:id, "session-form")
     find_within_element(form, :tag, "button")
     |> click
-    
+
     ## THEN ##
     assert current_path() == "/login"
     message = find_lement(:class, "alert-danger") |>
     visible_text()
     assert message == "Invalid username/password combinations"
+  end
+
+  test "successful login for valid credential" do
+    ## When ##
+    navigate_to("/login")
+    form = find_element(:id, "session-form")
+    find_within_element(form, :name, "session[email]")
+    |> fill_field("john@example.com")
+    find_within_element(form, :name, "session[password]")
+    |> fill_field("secret")
+    find_within_element(form, :tag, "button")
+    |> click
+    ## THEN ##
+    assert current_path() == "/"
+    message = find_element(:class, "alert-info")
+    |> visible_text()
+    assert message == "Login successful"
+  end
+
+  test "shows error message for invalid credentials" do
+    ## WHEN ##
+    navigate_to("/login")
+
+    form = find_element(:id, "session-form")
+
+    find_within_element(form, :tag, "button")
+    |> click
+
+    ## THEN ##
+    assert current_path() == "/login"
+    message = find_element(:class, "alert-danger") |>
+    visible_text()
+    assert message == "Invalid username/password combination"
   end
 end
