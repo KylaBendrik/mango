c47a9032679f7a12fa6c42f582034f9565fb0805
Beth Jackson <Mbeth.ashton@gmail.com>
2021-04-20 19:04:37 -0400

    Editing Ajax cart

--- assets/js/ajax_cart.js
+++ assets/js/ajax_cart.js
@@ -1,4 +1,24 @@
 // find the forms
-// for each one: Add some submit logic
-// While submitting: send the data ourselves
-// When the response comes back: Display a helpful message
+//  for each one: Add some submit logic
+//    While submitting: send the data ourselves
+//      When the response comes back: Display a helpful message
+
+const handleSubmission = function handlSubmission(submitEvent) {
+  submitEvent.preventDefault();
+
+  const post_url = submitEvent.target.action;
+
+  fetch('/cart')
+}
+
+const nodes = document
+  .querySelectorAll('.cart-form')
+  .forEach(form => {
+    form.addEventListener('submit', handleSubmission);
+  })
+
+// To display pretty messages:
+// Adding a message container
+// Adding a message to that container
+// Keeping track of the recent messages
+// Timing out stale messages
